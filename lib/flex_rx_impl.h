92c9e0b11aba4dca7374a0b677eb09fd4102ced2
hands <hands@email.arizona.edu>
2017-09-06 11:05:02 -0700

    Added internal performance matrix

--- lib/flex_rx_impl.h
+++ lib/flex_rx_impl.h
@@ -65,7 +65,7 @@ namespace gr {
       flex_rx_impl();
       ~flex_rx_impl();
 
-      void get_performance_matrix(){ return d_performance_matrix; };
+      performance_info get_performance_info(unsigned int modulation, unsigned int inner_code, unsigned int outer_code){ return d_performance_matrix[modulation][inner_code][outer_code]; };
 
       // Where all the action really happens
       int work(int noutput_items,

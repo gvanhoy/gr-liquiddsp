77ae18f48dfa425f6a974d0203516aae5a9c676c
gvanhoy <basebzombie@gmail.com>
2017-09-11 13:22:15 -0700

    Forcing usage of the GUI

--- apps/flex_transceiver_2.py
+++ apps/flex_transceiver_2.py
@@ -1,15 +1,46 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+##################################################
+# GNU Radio Python Flow Graph
+# Title: Top Block
+# Generated: Mon Sep 11 13:16:42 2017
+##################################################
+
+from PyQt4 import Qt
 from gnuradio import blocks
 from gnuradio import gr
-import time
 import es
 import liquiddsp
 import pmt
-import numpy as np
+import sys
+from gnuradio import qtgui
 
 
-class FlexTransceiver(gr.top_block):
-    def __init__(self, samp_rate=2000000):
-        gr.top_block.__init__(self, "Flex Transceiver")
+class FlexLoopback(gr.top_block, Qt.QWidget):
+
+    def __init__(self, samp_rate=1000000):
+        gr.top_block.__init__(self, "Top Block")
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("Top Block")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except:
+            pass
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "top_block")
+        self.restoreGeometry(self.settings.value("geometry").toByteArray())
 
         ##################################################
         # Variables
@@ -19,12 +50,12 @@ class FlexTransceiver(gr.top_block):
         ##################################################
         # Blocks
         ##################################################
-        self.liquiddsp_flex_tx_0 = liquiddsp.flex_tx(0, 0, 0)
+        self.liquiddsp_flex_tx_0 = liquiddsp.flex_tx(10, 1, 4)
         self.liquiddsp_flex_rx_0 = liquiddsp.flex_rx()
-        self.es_source_0 = es.source(1 * [gr.sizeof_gr_complex], 1, 2, 0)
-        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex * 1, samp_rate, True)
+        self.es_source_0 = es.source(1*[gr.sizeof_gr_complex], 1, 2, 0)
+        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate, True)
         self.blocks_random_pdu_0 = blocks.random_pdu(50, 50, chr(0xFF), 2)
-        self.blocks_message_strobe_0 = blocks.message_strobe(pmt.intern("TEST"), 1000)
+        self.blocks_message_strobe_0 = blocks.message_strobe(pmt.intern("TEST"), 100)
 
         ##################################################
         # Connections
@@ -35,6 +66,11 @@ class FlexTransceiver(gr.top_block):
         self.connect((self.blocks_throttle_0, 0), (self.liquiddsp_flex_rx_0, 0))
         self.connect((self.es_source_0, 0), (self.blocks_throttle_0, 0))
 
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "top_block")
+        self.settings.setValue("geometry", self.saveGeometry())
+        event.accept()
+
     def get_samp_rate(self):
         return self.samp_rate
 
@@ -42,42 +78,33 @@ class FlexTransceiver(gr.top_block):
         self.samp_rate = samp_rate
         self.blocks_throttle_0.set_sample_rate(self.samp_rate)
 
-    # '''
-    # For some reason, GNU Radio segfaults using this method quite often. I imagine because there's no control over
-    # how much is shoved into the internal queue at any point in time...
-    # '''
-    # def send_packet(self, modulation, inner_code, outer_code, payload):
-    #     self.liquiddsp_flex_tx_0.set_modulation(modulation)
-    #     self.liquiddsp_flex_tx_0.set_inner_code(inner_code)
-    #     self.liquiddsp_flex_tx_0.set_outer_code(outer_code)
-    #     msg = pmt.cons(pmt.PMT_NIL, pmt.to_pmt(np.array(payload, dtype=np.uint8)))
-    #     self.liquiddsp_flex_tx_0.to_basic_block()._post(pmt.intern("pdus"), msg)
 
+def main(top_block_cls=FlexLoopback, options=None):
 
-def main(top_block_cls=FlexTransceiver, options=None):
+    from distutils.version import StrictVersion
+    if StrictVersion(Qt.qVersion()) >= StrictVersion("4.5.0"):
+        style = gr.prefs().get_string('qtgui', 'style', 'raster')
+        Qt.QApplication.setGraphicsSystem(style)
+    qapp = Qt.QApplication(sys.argv)
 
     tb = top_block_cls()
     tb.start()
+    tb.show()
 
-    # for m in range(11):
-    #     for i in range(7):
-    #         for o in range(8):
-                # time.sleep(.01)
-                # tb.liquiddsp_flex_tx_0.set_modulation(m)
-                # tb.liquiddsp_flex_tx_0.set_inner_code(i)
-                # tb.liquiddsp_flex_tx_0.set_outer_code(o)
-                # print "MCS: " + str(m) + " " + str(i) + " " + str(o) + "rx: " + \
-                #       tb.liquiddsp_flex_rx_0.get_num_received(m, i, o) + "ok: " + \
-                #       tb.liquiddsp_flex_rx_0.get_num_correct(m, i, o)
+    def quitting():
+        tb.stop()
+        tb.wait()
 
-    try:
-        raw_input('Press Enter to quit: ')
-    except EOFError:
-        pass
-
-    tb.stop()
-    tb.wait()
+    qapp.connect(qapp, Qt.SIGNAL("aboutToQuit()"), quitting)
+    qapp.exec_()
 
 
 if __name__ == '__main__':
-    main()
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"

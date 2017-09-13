8af44d19a1e1b8baf08d28feec5b1b51d3ef18a7
gvanhoy <basebzombie@gmail.com>
2017-09-13 08:38:11 -0700

    Now configuring flexframegen

--- lib/flex_tx_impl.cc
+++ lib/flex_tx_impl.cc
@@ -181,10 +181,10 @@ namespace gr {
     }
 
     void flex_tx_impl::configure(pmt::pmt_t configuration){
-        std::cout << "Got modulation: " << pmt::to_long(pmt::dict_ref(configuration, pmt::mp("modulation"), pmt::PMT_NIL)) << std::endl;
         if(pmt::dict_has_key(configuration, pmt::mp("modulation"))) set_modulation(pmt::to_long(pmt::dict_ref(configuration, pmt::mp("modulation"), pmt::PMT_NIL)));
         if(pmt::dict_has_key(configuration, pmt::mp("inner_code"))) set_inner_code(pmt::to_long(pmt::dict_ref(configuration, pmt::mp("inner_code"), pmt::PMT_NIL)));
         if(pmt::dict_has_key(configuration, pmt::mp("outer_code"))) set_outer_code(pmt::to_long(pmt::dict_ref(configuration, pmt::mp("outer_code"), pmt::PMT_NIL)));
+        flexframegen_setprops(d_fg, d_fgprops);
     }
 
     void flex_tx_impl::send_pkt(pmt::pmt_t pdu){

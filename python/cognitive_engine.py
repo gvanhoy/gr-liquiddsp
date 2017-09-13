31cb7d389948c0a26cba8968c4216fbe1edbf218
gvanhoy <basebzombie@gmail.com>
2017-09-13 08:54:35 -0700

    Cleanup and attempt to add CE code

--- python/cognitive_engine.py
+++ python/cognitive_engine.py
@@ -21,6 +21,8 @@
 
 from gnuradio import gr
 import pmt
+from Database_Control import *
+from Reset_databases import *
 
 
 class cognitive_engine(gr.sync_block):
@@ -32,13 +34,31 @@ class cognitive_engine(gr.sync_block):
             name="cognitive_engine",
             in_sig=[],
             out_sig=[])
+        self.database = DatabaseControl()
         self.message_port_register_in(pmt.intern('packet_info'))
         self.set_msg_handler(pmt.intern('packet_info'), self.handler)
         self.message_port_register_out(pmt.intern('configuration'))
+        self.num_packets = 0
 
     def handler(self, packet_info):
-        new_configuration = pmt.make_dict()
-        new_configuration = pmt.dict_add(new_configuration, pmt.intern("modulation"), pmt.from_long(1))
-        new_configuration = pmt.dict_add(new_configuration, pmt.intern("inner_code"), pmt.from_long(1))
-        new_configuration = pmt.dict_add(new_configuration, pmt.intern("outer_code"), pmt.from_long(1))
-        self.message_port_pub(pmt.intern('configuration'), new_configuration)
+        self.num_packets += 1
+        modulation = pmt.dict_ref(packet_info, pmt.intern("modulation"), pmt.PMT_NIL)
+        inner_code = pmt.dict_ref(packet_info, pmt.intern("inner_code"), pmt.PMT_NIL)
+        outer_code = pmt.dict_ref(packet_info, pmt.intern("outer_code"), pmt.PMT_NIL)
+        config_id = modulation*11 + inner_code*7 + outer_code + 1
+        configuration = make_Conf(config_id, modulation, inner_code, outer_code)
+        config11 = Conf_map(modulation, inner_code, outer_code)
+        goodput = math.log(config11.constellationN, 2) * (float(config11.outercodingrate)) * (float(config11.innercodingrate)) * payload_valid[0]
+        self.database.write_configuration(configuration,
+                                          pmt.dict_ref(packet_info, pmt.intern("header_valid"), pmt.PMT_NIL),
+                                          pmt.dict_ref(packet_info, pmt.intern("payload_valid"), pmt.PMT_NIL),
+                                          goodput)
+
+        ce_configuration = EGreedy(self.num_packets, .01, 1)
+        if ce_configuration is not None:
+            new_configuration = pmt.make_dict()
+            new_ce_configuration = ce_configuration[0]
+            new_configuration = pmt.dict_add(new_configuration, pmt.intern("modulation"), pmt.from_long(new_ce_configuration.modulation))
+            new_configuration = pmt.dict_add(new_configuration, pmt.intern("inner_code"), pmt.from_long(new_ce_configuration.innercode))
+            new_configuration = pmt.dict_add(new_configuration, pmt.intern("outer_code"), pmt.from_long(new_ce_configuration.outercode))
+            self.message_port_pub(pmt.intern('configuration'), new_configuration)

9d84a143bbc4fdb956f18b71a6929ad58f692d3d
gvanhoy <basebzombie@gmail.com>
2017-09-13 08:08:03 -0700

    Symbol instead of mp

--- lib/flex_rx_impl.cc
+++ lib/flex_rx_impl.cc
@@ -230,12 +230,13 @@ namespace gr {
                     // pmt::pmt_t payload_pdu(pmt::cons(pmt::PMT_NIL, payload_pmt));
 
                     pmt::dict_add(packet_info, pmt::mp("header_valid"), pmt::from_long((long) d_info->_header_valid));
+                    std::cout << "Dict has key: header_valid " << pmt::dict_has_key(packet_info, pmt::mp("header_valid")) << std::endl;
+                    std::cout << "Dict has keys: " << pmt::dict_items(packet_info) << std::endl;
                     pmt::dict_add(packet_info, pmt::mp("payload_valid"), pmt::from_long((long) d_info->_payload_valid));
                     pmt::dict_add(packet_info, pmt::mp("modulation"), pmt::from_long((long) d_rx_mod_scheme));
                     pmt::dict_add(packet_info, pmt::mp("inner_code"), pmt::from_long((long) d_rx_inner_code));
                     pmt::dict_add(packet_info, pmt::mp("outer_code"), pmt::from_long((long) d_rx_outer_code));
 
-                    std::cout << "Dict has key: header_valid " << pmt::dict_has_key(packet_info, pmt::mp("header_valid")) << std::endl;
 
                     message_port_pub(pmt::mp("packet_info"), packet_info);
 

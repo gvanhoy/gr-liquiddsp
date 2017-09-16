b5736205556a6ff7309cdcd413a729af24b19ccf
gvanhoy <basebzombie@gmail.com>
2017-09-16 11:05:16 -0700

    Working now

--- python/cognitive_engine.py
+++ python/cognitive_engine.py
@@ -55,7 +55,7 @@ class cognitive_engine(gr.sync_block):
         outer_code = pmt.to_python(pmt.dict_ref(packet_info, pmt.intern("outer_code"), pmt.PMT_NIL))
         payload_valid = pmt.to_python(pmt.dict_ref(packet_info, pmt.intern("header_valid"), pmt.PMT_NIL))
         header_valid = pmt.to_python(pmt.dict_ref(packet_info, pmt.intern("payload_valid"), pmt.PMT_NIL))
-        config_id = modulation*7*8 + inner_code*8 + outer_code 
+        config_id = modulation*7*8 + inner_code*8 + outer_code
         configuration = ConfigurationMap(modulation, inner_code, outer_code, config_id)
         goodput = np.log2(configuration.constellationN) * (float(configuration.outercodingrate)) * (float(configuration.innercodingrate)) * payload_valid
         self.database.write_configuration(configuration,
@@ -364,7 +364,7 @@ class CognitiveEngine:
                 self.training_mode = False
             return config_map, config_map
 
-        for j in xrange(1, num_configs + 1):
+        for j in xrange(0, num_configs):
             self.config_cursor.execute('SELECT * FROM CONFIG WHERE ID=?', [j])
             for row in self.config_cursor:
                 Modulation = row[1]

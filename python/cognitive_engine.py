295b0b8d49e2f7b630bd3f69d6bac67977610575
gvanhoy <basebzombie@gmail.com>
2017-09-30 09:46:01 -0700

    modified the block

--- python/cognitive_engine.py
+++ python/cognitive_engine.py
@@ -108,6 +108,14 @@ class cognitive_engine(gr.sync_block):
                 self.initial_epsilon -= 0.001
         elif self.ce_type == "RoTA":
             ce_configuration = self.engine.RoTA(self.num_packets, self.Throughput_Threshold, self.PSR_Threshold, self.delayed_feedback)
+        elif self.ce_type == "meta":
+            SNratio = 10 * np.log10(np.power(0.05/(2*self.noise), 2))
+            if SNratio < 12:
+                ce_configuration = self.engine.epsilon_greedy(self.num_packets, epsilon, self.delayed_feedback)
+            elif SNratio < 18:
+                ce_configuration = self.engine.annealing_epsilon_greedy(self.num_packets, self.initial_epsilon, self.delayed_feedback)
+            else:
+                ce_configuration = self.engine.gittins(self.num_packets, DiscountFactor, self.delayed_feedback)
 
 
         if ce_configuration is not None:

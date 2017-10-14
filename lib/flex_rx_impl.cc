5e4dcd88359a63f0240984b219f44e4d63accd4d
gvanhoy <basebzombie@gmail.com>
2017-10-14 11:02:54 -0700

    Added extra output port

--- lib/flex_rx_impl.cc
+++ lib/flex_rx_impl.cc
@@ -57,6 +57,7 @@ namespace gr {
             }
         }
         message_port_register_out(pmt::mp("constellation"));
+        message_port_register_out(pmt::mp("payload_data"));
         message_port_register_out(pmt::mp("packet_info"));
     }
 
@@ -216,6 +217,11 @@ namespace gr {
                 pmt::pmt_t constellation_pdu(pmt::cons(pmt::PMT_NIL, constellation_pmt));
 
                 message_port_pub(pmt::mp("constellation"), constellation_pdu);
+
+                pmt::pmt_t payload_pmt = pmt::init_u8vector(d_info->_payload_len, d_info->_payload);
+                pmt::pmt_t payload_pdu(pmt::cons(pmt::PMT_NIL, payload_pmt));
+
+                message_port_pub(pmt::mp("payload_data"), payload_pdu);
 
                 if(d_info->_header_valid){
 

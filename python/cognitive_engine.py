5ef2c20ec8fa4878b4f06586f7c4288b91a37e4b
gvanhoy <basebzombie@gmail.com>
2017-10-17 10:32:14 -0700

    tweak

--- python/cognitive_engine.py
+++ python/cognitive_engine.py
@@ -87,8 +87,11 @@ class cognitive_engine(gr.sync_block):
         config_id = modulation*7*8 + inner_code*8 + outer_code + 1
         configuration = ConfigurationMap(modulation, inner_code, outer_code, config_id)
         goodput = np.log2(configuration.constellationN) * (float(configuration.outercodingrate)) * (float(configuration.innercodingrate)) * payload_valid
-        SNratio = 10 * np.log10(np.power(0.05 / (2 * dynamic_noise), 2))
-        print "SNR = ", SNratio
+        if dynamic_noise > 0:
+            SNratio = 10 * np.log10(np.power(0.05 / (2 * dynamic_noise), 2))
+            print "SNR = ", SNratio
+        else:
+            print "SNR = 0"
         if self.delayed_feedback == "no_delay":
             if modulation >= 0:
                 if inner_code >= 0:
@@ -145,27 +148,26 @@ class cognitive_engine(gr.sync_block):
             self.message_port_pub(pmt.intern('configuration'), new_configuration)
 
     def get_number(self):
-        if self.channel == "nonstationary":
-            if self.num_packets < 200:
-                global dynamic_noise
-                dynamic_noise = 0.0
-                return dynamic_noise
-            elif self.num_packets < 400:
-                global dynamic_noise
-                dynamic_noise = 0.006
-                return dynamic_noise
-            elif self.num_packets < 600:
-                global dynamic_noise
-                dynamic_noise = 0.02
-                return dynamic_noise
-            elif self.num_packets < 800:
-                global dynamic_noise
-                dynamic_noise = 0.01
-                return dynamic_noise
-            else:
-                global dynamic_noise
-                dynamic_noise = 0.0025
-                return dynamic_noise
+        if self.num_packets < 200:
+            global dynamic_noise
+            dynamic_noise = 0.0
+            return dynamic_noise
+        elif self.num_packets < 400:
+            global dynamic_noise
+            dynamic_noise = 0.006
+            return dynamic_noise
+        elif self.num_packets < 600:
+            global dynamic_noise
+            dynamic_noise = 0.02
+            return dynamic_noise
+        elif self.num_packets < 800:
+            global dynamic_noise
+            dynamic_noise = 0.01
+            return dynamic_noise
+        else:
+            global dynamic_noise
+            dynamic_noise = 0.0025
+            return dynamic_noise
 
 class DatabaseControl:
     def __init__(self):
